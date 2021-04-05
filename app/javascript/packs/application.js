5eb287da5cf69f5880d209f912bbc539e2032156
Siddharth-29 <siddharthagrawal2998@gmail.com>
2021-04-05 12:35:35 +0530

    jquery install and bugs resolve

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'cocoon'
+import "cocoon"
+// import 'jquery-ujs';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
+require("cocoon")
+require("jquery")
+// require("jquery_ujs")
