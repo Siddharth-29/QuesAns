87d8cde4460b6ecc9330b74e834c24853a67ae5a
Siddharth-29 <siddharthagrawal2998@gmail.com>
2021-04-05 12:35:35 +0530

    jquery install and bugs resolve

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :questions do 
     resources :answers
   end
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
